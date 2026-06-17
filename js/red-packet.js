1a7ed1be9a3a01ebae396ddf1f9c7eadc266c33e
panetfran <panettone12521@gmail.com>
2026-06-18 01:49:58 +0900

    Change red packet expiry check interval to 24 hours

--- js/red-packet.js
+++ js/red-packet.js
@@ -815,12 +815,12 @@
 
     console.log('[RedPacket] 模块加载完成 ✦');
 
-// 每隔 5 分钟自动检查一次有没有过期的红包
+// 每隔 24 小时自动检查一次有没有过期的红包
     setInterval(function() {
         if (typeof window.checkRedPacketExpiry === 'function') {
             window.checkRedPacketExpiry();
         }
-    }, 5 * 60 * 1000);
+    }, 24 * 60 * 60 * 1000);
 
     
 
